cca1c8e5964e18d7dfd077308f23064849d4c066
Samarth Rajput <ssamarth224@gmail.com>
2025-05-21 14:05:58 +0530

    scalable chat app

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 We are creating a distributed chat application
-Where we have multliple websocket servers, and multiple users can join any websocket server, they only need the roomId to subscribe to the roomId
+Where we have multiple websocket servers, and multiple users can join any websocket server, they only need the roomId to subscribe to the roomId
 This all will happen via pub sub 
 The architecture has been attached via an image 
+---
+This is how in a distributed websocket server, where 2 people are interested in the same room, are connected to different websocket servers can receive messages, without being stuck on a single server using pubsub
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
+import { createClient } from "redis";
 import { WebSocketServer, WebSocket } from "ws";
 
 // Intializing a websocket server that is going to run on port 8080
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8081 });
+
+// Creating a redis client
+// If you use createClient function, you cannot use this for both publishing and subscribing
+// client is only used to either publish or subscribe, that why we usually create 2 clients
+// Each backend server of ours, will need to subscribe to some rooms, will also need to publish to some rooms 
+const publishClient = createClient();
+publishClient.connect();
+
+const subscribeClient = createClient();
+subscribeClient.connect();
 
 // we will create a global variable called subscriptions, which is an object
 // whenever a user connects, we give them a random id, and this subscription variables stores, ki yaar this specific userId, has this websocket, this is there websocket connection
@@ -16,9 +27,9 @@ const subscriptions: { [key: string]: {
 
 
 // Creating an interval, that logs the subscriptions obejct, so that we can see whats it look like time to time
-setInterval(() => {
-    console.log(subscriptions);
-}, 5000);
+// setInterval(() => {
+//     console.log(subscriptions);
+// }, 5000);
 
 
 // Anytime a websocket connection that happens, any time a new user connects to this websocket server, this callback should be called 
@@ -49,11 +60,36 @@ wss.on("connection", function connection(userSocket) {
         // }
         if(parsedMessage.type === "SUBSCRIBE"){
             // and we store, the room that they want to be subscribed to, in the subscriptions object 
-            subscriptions[id].rooms.push(parsedMessage.room);
+            subscriptions[id].rooms.push(parsedMessage.room); 
+            // if one user is subscribed to the specific room, is when we need to do subscribeClient.subscribe()
+            // we have to do this once, when the firstPerson is interested to join the room 
+            // if this is the first person, who have subscribed to room1 on, this websocket server 
+            if(oneUserSubscribedTo(parsedMessage.room)){
+                console.log("subscribing on the pub sub to room " + parsedMessage.room);
+                subscribeClient.subscribe(parsedMessage.room, (message) => {
+                    const parsedMessage = JSON.parse(message);
+                    Object.keys(subscriptions).forEach((userId) => {
+                        const {ws, rooms} = subscriptions[userId];
+                        if(rooms.includes(parsedMessage.roomId)){
+                            ws.send(parsedMessage.message);
+                        }
+                    })
+                })
+            }       
         }
 
+        if(parsedMessage.type === "UNSUBSCRIBE"){
+            subscriptions[id].rooms = subscriptions[id].rooms.filter(x => x !== parsedMessage.room);
+            // if this the last person who left the room
+            if(lastPersonLeftRoom(parsedMessage.room)){
+                console.log("unsubscribing from the pub sub on room  " + parsedMessage.room);
+                subscribeClient.unsubscribe(parsedMessage.room);
+            }
+        }
+
+
         // whenever the user is sending a message to a room
-         // we expect the user to send us a message that looks something like this
+        // we expect the user to send us a message that looks something like this
         // {
         //      "type": "sendMessage",
         //      "roomId": "1",
@@ -62,20 +98,59 @@ wss.on("connection", function connection(userSocket) {
         if(parsedMessage.type === "sendMessage"){
             const message = parsedMessage.message;
             const roomId = parsedMessage.roomId;
-            // we need to iterate over all the current users(subscriptions)
-            Object.keys(subscriptions).forEach((userId) => { 
-                const {ws, rooms} = subscriptions[userId];
-                if (rooms.includes(roomId)){
-                    // forward the message to the other user 
-                    ws.send(message);
-                }
-            })
-        }
+            // // we need to iterate over all the current users(subscriptions)
+            // Object.keys(subscriptions).forEach((userId) => { 
+            //     const {ws, rooms} = subscriptions[userId];
+            //     if (rooms.includes(roomId)){
+            //         // forward the message to the other user 
+            //         ws.send(message);
+            //     }
+            // })
 
-        userSocket.send('hey you sent me this ' + data);
+            // that is the channel, i want to send the message on, we can only send strings when we are publishing to redis
+            // publish() function takes 2 arguments, roomId and message
+            publishClient.publish(roomId, JSON.stringify({
+                type: "sendMessage",
+                roomId: roomId,
+                message
+            }))
+
+        }
+        // userSocket.send('hey you sent me this ' + data);
     });
 
 });
+
+
+function oneUserSubscribedTo(roomId: string){
+    let totalTotalInterestedPeople = 0;
+    Object.keys(subscriptions).map(userId => {
+        if(subscriptions[userId].rooms.includes(roomId)){
+            totalTotalInterestedPeople++;
+        }
+    })
+    // This function is returning true, if only one person is subscribed to this room, it is returning false
+    // if more than one or 0 people are subscribed to this room
+    if(totalTotalInterestedPeople == 1){
+        return true;
+    }
+    return false;
+}
+
+
+function lastPersonLeftRoom(roomId: string){
+    let totalTotalInterestedPeople = 0;
+    Object.keys(subscriptions).map(userId => {
+        if(subscriptions[userId].rooms.includes(roomId)){
+            totalTotalInterestedPeople++;
+        }
+    })
+    if(totalTotalInterestedPeople == 0){
+        return true;
+    }
+    return false;
+}
+
 
 
 function randomId() {
